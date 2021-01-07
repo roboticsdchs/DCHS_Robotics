ddf10bb5df4957466f9f8737ed64fa75c9b7c25c
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2021-01-07 13:58:27 -0500

    switch to python 3

--- Hardware_tests/bi_dir_serial_comm/pi_bi_dir_serial_comm.py
+++ Hardware_tests/bi_dir_serial_comm/pi_bi_dir_serial_comm.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import serial
 import sys
 import time
 
 if __name__ == '__main__':
-  ser = serial.Serial('/dev/ttyACM-1', 9600, timeout=1)
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
   ser.flush()
-    
+  
   while True:
     ser.write(b"Hello from Raspberry Pi!\n")
-    line=ser.readline().decode('utf-9').rstrip()
+    line=ser.readline().decode('utf-8').rstrip()
     print(line) 
