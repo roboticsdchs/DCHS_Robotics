555562013b83054a62d1ab861b3cb24a8898624e
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2021-03-04 14:15:46 -0500

    pyfirmata updates

--- lib/drive_pkg/pi_drive.py
+++ lib/drive_pkg/pi_drive.py
@@ -7,42 +7,47 @@ import sys
 # what does true_speed do? do we need to include it in anything?
 
 # designed to work with rev spark motor controller and servo to steer the car
+
+
+# switch to .writeMicroseconds so that we can use docs on rev spark
+
+# switch to pymata4 because it has docs
   
 # forward drive commands
 def forward(drive_board_port, drive_pin, turn_pin, forward_angle, delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO 
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(2000)
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(254)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(forward_angle)
   sleep(delay)
 
 # backward drive commands 
 def backward(drive_board_port, drive_pin, turn_pin, backward_angle, delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(1000)
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(70)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(backward_angle)
   sleep(delay)
 
 # left drive commands
 def left(drive_board_port, drive_pin, turn_pin, left_angle, left_delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(2000)
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(110)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(left_angle)
   sleep(delay)
 
 # right drive commands
 def right(drive_board_port, drive_pin, turn_pin, right_angle, right_delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(2000)
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(110)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(right_angle)
   sleep(delay)
 
 # general drive commands 
 def drive(drive_board_port, drive_pin, turn_pin, motor_speed, steering_angle, delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(motor_speed)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(steering_angle)
@@ -50,9 +55,9 @@ def drive(drive_board_port, drive_pin, turn_pin, motor_speed, steering_angle, de
 
 
 def coast(drive_board_port, drive_pin, turn_pin, steering_angle, delay):
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = PWM
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].mode = SERVO
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].mode = SERVO
-  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(1220)
+  pyfirmata.Arduino('"' + drive_board_port + '"').digital[drive_pin].write(100)
   pyfirmata.Arduino('"' + drive_board_port + '"').digital[turn_pin].write(steering_angle)
   sleep(delay)
 
