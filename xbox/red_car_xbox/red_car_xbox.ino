ff560bb1ae13a9333090da5125e5fd299e220bda
N1H1L0 <74982254+N1H1L0@users.noreply.github.com>
2022-09-08 09:56:20 -0400

    red_car_xbox

--- xbox/red_car_xbox/red_car_xbox.ino
+++ xbox/red_car_xbox/red_car_xbox.ino
@@ -8,13 +8,13 @@ USB Usb;
 XBOXRECV Xbox(&Usb);
 
 /* Define the rotational speed of the motor. MUST be between 0 and 255. */
-int Motorpin = 6;
-int Turnpin = 5;
+int Motorpin = 6; //
+int Turnpin = 5; //
 int Stick = 0;
 int Speed=1;
 int Turn=1;
 int ActualSpeed = 90; // servo write of stop
-int ActualTurn = 13; // degree of straight
+int ActualTurn = 87; // degree of straight
 int NoTurn = 1;
 int i;
 
@@ -37,7 +37,7 @@ void setup() {
 void loop() {
   NoTurn = 1;
   Usb.Task(); 
-  Speed = map((Xbox.getButtonPress(R2, 0)-Xbox.getButtonPress(L2,0)), -255, 255, 0, 180);
+  Speed = map((Xbox.getButtonPress(L2, 0)-Xbox.getButtonPress(R2,0)), -255, 255, 0, 180);
   Stick = Xbox.getAnalogHat(LeftHatX, 0);
   if(i%20==0){
     if(Stick<-7500){
@@ -45,11 +45,11 @@ void loop() {
     NoTurn = 0;
   }
   if(Stick>7500){
-    Turn = map(Stick,7501, 32767, 86, 95);
+    Turn = map(Stick,7501, 32767, 86, 100);
     NoTurn = 0;
   }
   if(NoTurn>0){
-    Turn = 86;
+    Turn = 87;
   }
   if (Speed > 85){
     //Speed = map(Xbox.getButtonPress(R2, 0), 0, 255, 90, 180);
@@ -60,18 +60,18 @@ void loop() {
   else if(ActualSpeed<Speed){
     ActualSpeed = ActualSpeed +3;
   }
-  if((ActualSpeed<92&&ActualSpeed>88)||(Xbox.getButtonPress(R2, 0)<3&&Xbox.getButtonPress(L2, 0)<3)){
+  if((ActualSpeed<92&&ActualSpeed>88)||(Xbox.getButtonPress(L2, 0)<3&&Xbox.getButtonPress(R2, 0)<3)){
     ActualSpeed = 90;
   }
   Serial.print("Turn = ");
-  if(Turn > 26){
-    Turn = 26;
-  }
-  else if(Turn < 0){
-    Turn = 0;
-  }
   Serial.print(Turn);
   Serial.print("Speed = ");
+  if(ActualSpeed> 110){
+    ActualSpeed = 105;
+  }
+  else if(ActualSpeed< 75){
+    ActualSpeed = 75;
+  }
   Serial.println(ActualSpeed);
   Move.write(ActualSpeed);
   Turning.write(Turn);
